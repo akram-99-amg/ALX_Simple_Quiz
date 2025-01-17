9adc929dd697775cf04e3bc0a44fa2de577d624c
Akram <mazakram04@gmail.com>
2025-01-17 09:14:46 -0500

    update quiz

--- quiz.js
+++ quiz.js
@@ -1,9 +1,9 @@
 function checkAnswer() {
     let correctAnswer = "4";
-    let userAnswer = document.querySelector("[name='quiz']").value;
-
-    if (userAnswer == correctAnswer) {
-        let feedback = document.getElementById("feedback")
+    let userAnswer = document.querySelector("input[name='quiz']:checked").value
+    let feedback = document.getElementById("feedback")
+    if (userAnswer === correctAnswer) {
+        
         feedback.textContent = "Correct! Well done.";
 
     } else {
